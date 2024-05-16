920a79a3e9beca8943755bd07d230f623f020aac
jatinderkumar <Jatinder.Kumar@tabcorp.com.au>
2024-05-16 14:54:52 +0530

    ADDED :: Github actions with app distribution.

---
+++ build_and_release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# shellcheck disable=SC2164
+cd android
+echo "fastlane test Started..."
+#fastlane android test --env aus
+bundle install
+bundle update fastlane
+bundle exec fastlane android test
+echo "fastlane test completed."
